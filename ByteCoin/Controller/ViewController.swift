6184ca85ebdddfd872b0b0c0838653cf50e880db
michaeltaranik <taranikmichael32@gmail.com>
2024-07-08 13:57:01 +0200

    pickerView implemented

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,11 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UITextFieldDelegate, UIPickerViewDelegate {
 
+    
+
+    
+    @IBOutlet var bitcoinLabel: UILabel!
+    @IBOutlet var currencyLabel: UILabel!
+    @IBOutlet var pickerView: UIPickerView!
+    var textField = UITextField()
+    let coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        textField.delegate = self
+//        buttonB.layer.cornerRadius = CGFloat(GLfloat(floatLiteral: 0.0))
+//        buttonB.clipsToBounds = false
+    }
+    
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        currencyLabel.text = coinManager.currencyArray[row]
     }
 
 
